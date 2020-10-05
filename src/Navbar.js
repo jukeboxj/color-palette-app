e0d8b0b6b06ddf1d29320c6b991974c44ee2ed95
jukeboxj <skyyy.chen@gmail.com>
2020-10-04 22:49:15 -0400

    snack-bar added

---
+++ src/Navbar.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import './Navbar.css';
+
+export default class Navbar extends Component {
+
+    state = { open : false };
+    
+    handleChange = evt => {
+        this.props.changeFormat(evt.target.value);
+        this.setState({ open : true });
+    }
+
+    handleClose = evt => {
+        this.setState({ open : false })
+    }
+
+    render() {
+        const {level, format, changeLevel} = this.props;
+        const {open} = this.state;
+
+        return (
+            <header className='Navbar'>
+                <div className='logo'>
+                    <a href='#'>reactColorPicker</a>
+                </div>
+                <div className="nav-slider">
+                    <span>Level: {level}</span>
+                    <div className='slider'>
+                        <Slider min={100}
+                            max={900}
+                            step={100}
+                            onAfterChange={changeLevel}
+                            defaultValue={level} />
+                    </div>
+                </div>
+                <div className='select-container'>
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+                        <MenuItem value='rgb'>RBG - rbg(255,255,255)</MenuItem>
+                        <MenuItem value='rgba'>RBGA - rbga(255,255,255,1.0)</MenuItem>
+                    </Select>
+                </div>
+                <Snackbar 
+                    anchorOrigin={{ vertical:"bottom", horizontal:"left" }}
+                    open={open}
+                    autoHideDuration={3000}
+                    message={<span id='message-id'>Format Changed to {format.toUpperCase()}</span>}
+                    ContentProps={{'aria-describedby': 'message-id'}}
+                    onClose={this.handleClose}
+                    action={
+                        <IconButton 
+                            size="small" 
+                            aria-label="close" 
+                            color="inherit" 
+                            onClick={this.handleClose}>
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    } />
+            </header>
+        )
+    }
+}
--- src/Palette.js
+++ src/Palette.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css';
 
 export default class Palette extends Component {
-    render() {
-        const colorBoxes = this.props.colors.map(c => 
-            <ColorBox bg={c.color} name={c.name} />
-        );
+    
+    state = { 
+        level : 500,
+        format : 'hex' 
+    };
 
+    changeLevel = lvl => {
+        this.setState({ level: lvl })
+        // console.log(lvl)
+    }
+
+    changeFormat = fmt => {
+        this.setState({ format: fmt })
+    }
+    
+    render() {
+        const { colors } = this.props.palette;
+        const { level, format } = this.state;
+
+        const colorBoxes = colors[level].map(c => 
+            <ColorBox bg={c[format]} name={c.name} />
+        );
 
         return (
             <div className="Palette">
-                <div className="Palette-nav"></div>
+                <Navbar 
+                    level={level}
+                    format={format} 
+                    changeLevel={this.changeLevel}
+                    changeFormat={this.changeFormat} />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
