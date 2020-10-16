1ae903e14e056ab443753f359ff64ce9943a7891
jukeboxj <skyyy.chen@gmail.com>
2020-10-15 23:05:26 -0400

    JSS styled in
    Mini-color page

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,19 +1,61 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-    main: {
-        backgroundColor : 'purple',
-        border: '3px solid teal'
+    root : {
+        backgroundColor : 'white',
+        borderRadius : '5px',
+        padding : '.5rem',
+        position : 'relative',
+        overflow : 'hidden',
+        '&:hover': {
+            cursor : 'pointer'
+        }
+    },
+    colors : {
+        backgroundColor : '#dae1e4',
+        height : '150px',
+        width : '100%',
+        borderRadius : '5px',
+        overflow : 'hidden'
+    },
+    title : {
+        display : 'flex',
+        justifyContent : 'space-between',
+        alignItems : 'center',
+        margin : '0',
+        color : 'black',
+        paddingTop : '.5rem',
+        fontSize : '1rem',
+        position : 'relative'
+    },
+    emoji : {
+        marginLeft : '.5rem',
+        fontSize : '1.5rem'
+    },
+    miniColor : {
+      height : '25%',
+      width : '20%',
+      display : 'inline-block',
+      margin : '0 auto',
+      position : 'relative',
+      marginBottom : '-3.5px'
     }
 }
 
 const MiniPalette = props => {
+    const { classes, paletteName, emoji, colors} = props;
+    const miniColorBoxes = colors.map(c => 
+        <div 
+            className={classes.miniColor} 
+            style={{ backgroundColor : c.color }}
+            key={c.name} />)
+
     return (
-        <div>
-            <h1>MiniPalette is imported</h1>
+        <div className={classes.root}>
+            <div className={classes.colors}>{miniColorBoxes}</div>
+            <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
         </div>
     )
 }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-import MiniPalette from './MiniPalette';
-import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
 
-export default class PaletteList extends Component {
+import MiniPalette from './MiniPalette';
+
+const styles = {
+    root : {
+        backgroundColor : 'blue',
+        height : '100vh',
+        display : 'flex',
+        alignItems : 'flex-start',
+        justifyContent : 'center'
+    },
+    container : {
+        width : '50%',
+        display : 'flex',
+        alignItems : 'flex-start',
+        flexDirection : 'column',
+        flexWrap : 'wrap'
+    },
+    nav : {
+        display : 'flex',
+        width : '100%',
+        justifyContent: 'space-between',
+        color : 'white'
+    },
+    palettes : {
+        boxSizing : 'border-box',
+        width : '100%',
+        display : 'grid',
+        gridTemplateColumns : 'repeat(3, 30%)',
+        gridGap : '5%'
+    }
+}
+
+export class PaletteList extends Component {
     render() {
-        const { palettes } = this.props;
-        console.log(palettes);
+        const { palettes, classes } = this.props;
         return (
-            <div>
-                <MiniPalette />
-                <h1>React Colors</h1>
-                <ul>
-                    {palettes.map(p =>
-                        <li>
-                            <Link to={`/${p.id}`}>{p.paletteName}</Link>
-                        </li>)}
-                </ul>
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map(p => <MiniPalette key={p.id} {...p} />)}
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default withStyles(styles)(PaletteList);
