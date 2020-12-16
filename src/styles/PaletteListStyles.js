b7559aa1169525a91290b84816de737f27a84497
jukeboxj <skyyy.chen@gmail.com>
2020-12-15 23:16:16 -0500

    Made Palettelist Page responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
     root: {
         backgroundColor: 'blue',
@@ -11,7 +13,19 @@ export default {
         display: 'flex',
         alignItems: 'flex-start',
         flexDirection: 'column',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        [sizes.down('lg')]: {
+            width: '65%',
+        },
+        // [sizes.down('md')]: {
+        //     width: '70%',
+        // },
+        // [sizes.down('sm')]: {
+        //     width: '80%',
+        // },
+        // [sizes.down('xs')]: {
+        //     width: '90%',
+        // },
     },
     nav: {
         display: 'flex',
@@ -28,6 +42,14 @@ export default {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+        gridGap: '5rem',
+        [sizes.down('md')]: {
+            gridTemplateColumns: 'repeat(2, 50%)',
+            gridGap: '3rem',
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: 'repeat(1, 100%)',
+            gridGap: '1rem',
+        },
     }
 }
